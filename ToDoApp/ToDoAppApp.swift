6fd3039822de15f6a0b1659101af6293252168c5
Baltazar Servín <thebaltazarsr@gmail.com>
2025-10-26 14:46:09 -0600

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Baltazar Servin on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
